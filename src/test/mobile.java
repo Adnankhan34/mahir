4ebad6187b81b617edeba2e641801337af74bf1a
ADNAN KHAN <ADNAN KHAN@192.168.10.7>
2016-06-19 11:40:48 -0700

    i commited

---
+++ src/test/mobile.java
@@ -0,0 +1,18 @@
+package test;
+
+public class mobile {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	System.out.println("COmats");
+	
+	}
+	
+	
+	
+
+}
